c97be9d39d1f27234858d6a1ee14c94911c266a9
tobazan <bazanme90@gmail.com>
2021-04-11 10:41:30 -0300

    testeo propaga.py con los casos propuestos por los profes OK

--- Clase04/propaga.py
+++ Clase04/propaga.py
@@ -26,10 +26,15 @@ def propagar(lista):
     return lista
 
 
-print(propagar([ 0, 0, 0,-1, 1, 0, 0, 0,-1, 0, 1, 0, 0]))
+#
+lista_1 = [ 0, 0, 0, 1, 0, 0]
+lista_2 = [ 0, 0, 0, 0, 0, -1]
+lista_3 = [ 0, 0, 0, 0, 0, 1]
+lista_4 = []
+lista_5 = [ 0 for _ in range(1000) ] + [1]
+lista_6 = [1] + [ 0 for _ in range(1000) ]
+lista_7 = [ (i% 6)//2-1 for i in range(200) ]
+lista_8 = [ -1*((i% 6)//2-1) for i in range(60) ]
+#
 
-print(propagar([ 0, 0, 0, 1, 0, 0]))
-
-print(propagar([ -1, 0, 0, 1, 0, -1]))
-
-print(propagar([ 0, -1, 0, 1, 0, 0]))
+propagar(lista_8)
